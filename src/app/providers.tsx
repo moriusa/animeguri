0d1379e70dfa5a78a195599c039ff62c07354be0
morikans <shirousagi.morikan@gmail.com>
2025-09-06 02:28:08 +0900

    save

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,15 +2,18 @@
 
 import { AuthProvider } from "react-oidc-context";
 import React from "react";
+import { WebStorageStateStore } from "oidc-client-ts";
 
 // Cognitoの設定（クライアント側）
 const cognitoAuthConfig = {
   authority: "https://cognito-idp.ap-northeast-1.amazonaws.com/ap-northeast-1_IXZ6Ws2y8",
   client_id: "3ieiqhe71jkq813kta7i1onqio",
-  redirect_uri: "https://anipilgoo.web.app/",
+  redirect_uri: "http://localhost:3000",
   response_type: "code",
   scope: "phone openid email",
   extraQueryParams: { lang: 'ja' },
+  // LocalStorageにセッション情報を保存
+  userStore: new WebStorageStateStore({ store: window.localStorage }),
 };
 
 export function OidcProvider({ children }: { children: React.ReactNode }) {
