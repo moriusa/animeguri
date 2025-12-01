ed96e09b13b5c85d26fddb0a5a195949fe4b3000
morikans <shirousagi.morikan@gmail.com>
2025-12-02 04:59:18 +0900

    supabaseのinitを切り分け

--- packages/cdk/lambda/getUserProfile/index.ts
+++ packages/cdk/lambda/getUserProfile/index.ts
@@ -1,63 +1,11 @@
 import { GetObjectCommand, S3Client } from "@aws-sdk/client-s3";
-import { SSMClient, GetParameterCommand } from "@aws-sdk/client-ssm";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
-import { createClient, type SupabaseClient } from "@supabase/supabase-js";
+import { type SupabaseClient } from "@supabase/supabase-js";
 import { APIGatewayProxyEventV2 } from "aws-lambda";
+import { initSupabase } from "../common/supabaseClient";
 
 const s3Client = new S3Client({});
-const ssmClient = new SSMClient({});
 const bucketName = process.env.S3_BUCKET_NAME!;
-const supabaseUrlParamName = process.env.SUPABASE_URL!;
-const supabaseAnonKeyParamName = process.env.SUPABASE_ANON_KEY!;
-let supabase: SupabaseClient | null = null;
-
-// ssmからsupabase認証情報取得
-const getSupabaseConfig = async () => {
-  console.log("getSupabaseConfig: start", {
-    supabaseUrlParamName,
-    supabaseAnonKeyParamName,
-  });
-  const [urlRes, keyRes] = await Promise.all([
-    ssmClient.send(
-      new GetParameterCommand({
-        Name: supabaseUrlParamName,
-        WithDecryption: false,
-      })
-    ),
-    ssmClient.send(
-      new GetParameterCommand({
-        Name: supabaseAnonKeyParamName,
-        WithDecryption: true,
-      })
-    ),
-  ]);
-  console.log("getSupabaseConfig: got params");
-  return {
-    supabaseUrl: urlRes.Parameter?.Value,
-    supabaseAnonKey: keyRes.Parameter?.Value,
-  };
-};
-
-const initSupabase = async () => {
-  if (supabase) return supabase;
-  console.log("initSupabase: using cached client");
-
-  const { supabaseUrl, supabaseAnonKey } = await getSupabaseConfig();
-
-  console.log("initSupabase: fetching config from SSM");
-
-  try {
-    if (!supabaseUrl || !supabaseAnonKey)
-      throw new Error("Supabase URL or anon key is missing");
-    console.log("initSupabase: creating client");
-    supabase = createClient(supabaseUrl, supabaseAnonKey);
-    console.log("initSupabase: client created");
-    return supabase;
-  } catch (error) {
-    console.error("Failed to initialize Supabase:", error);
-    throw error;
-  }
-};
 
 // Supabaseからユーザー情報取得
 const getUserProfile = async (
