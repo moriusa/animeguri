7036e958adb4be4b2cae3de4c8eb9ffb17b2569d
morikans <shirousagi.morikan@gmail.com>
2026-05-30 21:04:52 +0900

    問い合わせフォームにgoogleフォーム埋め込み

--- apps/src/app/contact/page.tsx
+++ apps/src/app/contact/page.tsx
@@ -1,9 +1,15 @@
 const Page = () => {
   return (
-    <div className="p-10">
-      <div className="bg-white px-10 py-10 text-center">
+    <div className="md:p-10 max-w-3xl mx-auto">
+      <div className="bg-white md:px-4 py-10 text-center">
         <p className="font-bold text-xl">お問い合わせ</p>
-        <p className="mt-10">- コンテンツ準備中です -</p>
+        <iframe
+          src="https://docs.google.com/forms/d/e/1FAIpQLSdzRZEy7I-0I19djqLhmc06FXQfuH3DNMbxkqvDRiyBHAZurA/viewform?embedded=true"
+          height="1100"
+          className="w-full mt-8"
+        >
+          読み込んでいます…
+        </iframe>
       </div>
     </div>
   );
